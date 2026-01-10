9f6ee160980ef6f343e62da4fffb1d24494b4a93
silas <silas@enzler.info>
2026-01-10 21:28:14 +0100

    home setup in progress

--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def hello_partial(request):
+    return HttpResponse("""
+        <div class="p-3 bg-green-100 border border-green-300 rounded">
+            HTMX response loaded without a page reload 🚀
+        </div>
+    """)
